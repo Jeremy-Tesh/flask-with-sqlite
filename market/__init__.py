27486c4c08224045727e9b79f864d092513e9f41
Jeremy Tesh <jermitesh20@gmail.com>
2022-04-08 12:51:41 +0300

    feat:flask native form with html

--- market/__init__.py
+++ market/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
+app.config["SECRET_KEY"] = "ee6ecc99982738018394de26"
 
 db = SQLAlchemy(app)
 
--- market/routes.py
+++ market/routes.py
@@ -5,6 +5,7 @@ from market import app
 from flask import redirect, render_template
 from market.model import Item
 from market import db
+from market.forms import RegisterForm
 
 
 @app.route("/")
@@ -13,26 +14,32 @@ def home_page():
     return render_template("home.html")
 
 
-@app.route("/features")
-def features():
+@app.route("/products")
+def product_page():
     items = Item.query.all()
 
     return render_template("market.html", items=items)
 
 
-@app.route("/add", methods=["GET", "POST"])
-def add():
-    return render_template("add.html")
-    # item_name = request.form["name"]
-    # item_price = request.form["price"]
-    # item_barcode = request.form["barcode"]
-    # item_description = request.form["description"]
-    # new_item = Item(
-    #     name=item_name,
-    #     price=item_price,
-    #     barcode=item_barcode,
-    #     description=item_description,
-    # )
-    # db.session.add(new_item)
-    # db.session.commit()
-    # return redirect("/features")
+@app.route("/register")
+def register_page():
+    form = RegisterForm()
+    return render_template("register.html", form=form)
+
+
+# @app.route("/add", methods=["GET", "POST"])
+# def add():
+#     return render_template("add.html")
+# item_name = request.form["name"]
+# item_price = request.form["price"]
+# item_barcode = request.form["barcode"]
+# item_description = request.form["description"]
+# new_item = Item(
+#     name=item_name,
+#     price=item_price,
+#     barcode=item_barcode,
+#     description=item_description,
+# )
+# db.session.add(new_item)
+# db.session.commit()
+# return redirect("/features")
